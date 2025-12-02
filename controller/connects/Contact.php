df6a0e9a0a746cd6b2e188049cddac2b0d115ce7
sonam dorji <sonamdorji@Sonams-MacBook-Pro.local>
2025-12-02 10:43:34 +0600

    Filter removed

--- controller/connects/Contact.php
+++ controller/connects/Contact.php
@@ -7,7 +7,7 @@ class Contact extends BaseModel
     public function __construct()
     {
         parent::__construct();
-        $this->table = "contact_messages";
+        $this->table = "booking";
     }
 
     public function saveMessage(array $data): bool
