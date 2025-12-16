08a58ba1dcbdfc6df954b60d8990ad7cfa8d2b92
allay <suraceallay@gmail.com>
2025-12-16 12:58:29 +0600

    updated env

--- includes/Database.php
+++ includes/Database.php
@@ -1,14 +1,7 @@
 <?php
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
-    if (class_exists('\Dotenv\Dotenv')) {
-        try {
-            $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-            $dotenv->safeLoad();
-        } catch (Throwable $e) {
-            // ignore dotenv errors; fall back to environment` --- IGNORE ---
-        }
-    }
+    require_once __DIR__ . '/ConfigLoader.php';
 }
 
 class Database
@@ -23,11 +16,11 @@ class Database
 
     public function __construct()
     {
-        $this->host = getenv('DB_HOST') ?: '';
-        $this->db = getenv('DB_NAME') ?: '';
-        $this->user = getenv('DB_USER') ?: '';
-        $this->pass = getenv('DB_PASS') ?: '';
-        $this->charset = getenv('DB_CHARSET') ?: 'utf8mb4';
+        $this->host = ConfigLoader::env('DB_HOST') ?: '';
+        $this->db = ConfigLoader::env('DB_NAME') ?: '';
+        $this->user = ConfigLoader::env('DB_USER') ?: '';
+        $this->pass = ConfigLoader::env('DB_PASS') ?: '';
+        $this->charset = ConfigLoader::env('DB_CHARSET') ?: 'utf8mb4';
 
         $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
 
